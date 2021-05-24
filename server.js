ba1ca69108b3d0d8f61dd471f811abb404444cf4
Putta Prakash <putta.prakash@puprakash@merklinc.com>
2021-05-24 09:07:09 +0530

    Implemented app.use() function

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const express = require('express');
+const app = express();
+app.use(express.static(__dirname));
+
+var server = app.listen(8080,()=>{
+    console.log("Server is listening on port", server.address().port);
+});
