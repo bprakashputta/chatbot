cc70dfb9ed5fee9c00d8303d89ad527d5b2213a1
Putta Prakash <putta.prakash@puprakash@merklinc.com>
2021-05-31 16:04:07 +0530

    Implemented messages call

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
 
+var messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Bob', message: 'Hello'}
+]
+
+app.get('/messages', (request, response) => {
+    response.send(messages);
+})
+
 var server = app.listen(8080,()=>{
     console.log("Server is listening on port", server.address().port);
 });
